561315ac85eefa0651ecd699051534a6be7dbca2
cardinaldeacre <cardinaldeacre@gmail.com>
2025-07-30 13:38:06 +0700

    update logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,11 +3,11 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">GetEvent</span>
+            <div className="flex w- items-center justify-center bg-indigo-700 rounded-sm py-1 px-2 text-sidebar-primary-foreground">
+                <AppLogoIcon />
+                <div className="ml-1 grid flex-1 text-left text-lg">
+                    <span className="mb-0.5 truncate leading-tight font-semibold">GetEvent</span>
+                </div>
             </div>
         </>
     );
